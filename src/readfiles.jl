e153855ecd453db3d3edede90f31af371afbf881
Roger Herikstad <roger.herikstad@gmail.com>
2014-08-21 18:57:11 +0800

    Add area designation

--- src/readfiles.jl
+++ src/readfiles.jl
@@ -1,4 +1,6 @@
 import MAT
+import Base.parse
+using DataFrames
 
 function loadWaveformsFile(fname::String;time_conversion::Real=0.001)
 	fid = open(fname,"r")
@@ -21,12 +23,42 @@ function loadWaveformsFile(fname::String;time_conversion::Real=0.001)
 	return float(waveforms),float(timestamps)*time_conversion
 end
 
+function assign_area(sptrains::Dict,config;return_mapping=false)
+	newsptrains = Dict()
+	mapping = Dict()
+	rr = r"g([0-9]*)c([0-9])*[s]*"
+	for (k,v) in sptrains
+		m = match(rr,k)
+		group = int(m.captures[1])
+		cellnr = int(m.captures[2])
+		ad = filter((kk,vv)->group in vv, config)
+		aa = collect(keys(ad))[1]
+		cc = collect(values(ad))[1]
+		cidx = find(cc.==group)[1]
+		kname = "$(aa)_g$(cidx)c$(cellnr)"
+		newsptrains[kname] = v
+		mapping[k] = kname
+	end
+	if return_mapping
+		return newsptrains,mapping
+	else
+		return newsptrains
+	end
+end
+
 function getspiketrains(;session::String="",groups::Array{Int64,1}=Array(Int64,0),checkArtifact::Bool=true,verbose::Integer=0)
 	if isempty(groups)
 		#get the groups from the waveforms files in the current directory
 		files = split(chomp(readall(`find . -name "*waveforms.bin"`)))
 	else
 		files = []
+	end
+	config = Dict()
+	for ss in (".", "..")
+		if isfile("$(ss)/channel_config.csv")
+			config = channel_config("$(ss)/channel_config.csv")
+			break
+		end
 	end
 	SS = pmap(f->begin
 					m = match(r"([A-Za-z0-9_]*)g([0-9]*)",f)
@@ -35,7 +67,14 @@ function getspiketrains(;session::String="",groups::Array{Int64,1}=Array(Int64,0
 					sptrains = getspiketrains(session,group;checkArtifact=checkArtifact,verbose=verbose)
 					sptrains
 			end,files)
-	return merge(SS...)
+	sptrains = merge(SS...)
+	#TODO: replace 'groupXXX' with the appropriate area and array indication, i.e. vFEFAch1c01s
+	if !isempty(config)
+		newsptrains = assign_area(sptrains,config)
+	else
+		newsptrains = sptrains
+	end
+	return newsptrains
 end
 
 function getspiketrains(session::String, group::Integer;checkArtifact::Bool=true,verbose::Integer=0)
@@ -72,4 +111,20 @@ function getspiketrains(session::String, group::Integer;checkArtifact::Bool=true
 		end
 	end
 	return sptrains
+end
+
+function parse{T<:Integer}(::Type{Range{T}},a::String)
+	m = match(r"([0-9]*)[\-\:]([0-9]*)",a)
+     mc = m.captures
+    isempty(mc) && return nothing
+	ll = map(int,mc)
+    return range(ll[1],ll[2]-ll[1]+1)
+end
+
+function channel_config(fname::String)
+	dd = DataFrames.readtable(fname)	
+	#convert to ranges
+	q = map(x->parse(Range{Int64},x),dd[:channels])
+	dd[:channels] = q
+	return Dict(dd[:area],dd[:channels])
 end
