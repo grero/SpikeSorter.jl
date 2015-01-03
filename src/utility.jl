637a6fc4cb07e0292d41dc0e9cbd31b1b982cbfb
Roger Herikstad <roger.herikstad@gmail.com>
2015-01-03 19:16:52 +0800

    Added functions for getting info about template files

--- src/utility.jl
+++ src/utility.jl
@@ -1,3 +1,5 @@
+import HDF5
+import Base.isvalid
 
 create_template(nstates::Integer) = create_template(nstates, 1.0, 0.8, 0.2)
 function create_template(nstates::Integer, a::Real, b::Real, c::Real)
@@ -21,3 +23,45 @@ function create_signal(N::Integer, sigma::Number, nspikes::Integer, template::Ar
     end
     return S
 end
+
+function is_TemplateFile(fname::String)
+	if !HDF5.ishdf5(fname)	
+		return false
+	end
+	ff = HDF5.h5open(fname) 
+	_names = names(ff)
+	close(ff)
+	if !("spikeForms" in _names)
+		return false
+	end
+	return true
+end
+
+function is_valid_template_file(fname::String)
+	return get_valid_templates(fname) != nothing
+end
+
+function get_valid_templates(fname::String)
+	tf = TemplateFile(fname)
+	if tf == nothing
+		return nothing
+	end
+	return get_valid_templates(tf)
+end
+
+function get_valid_templates(tf::TemplateFile)
+	vidx = zeros(Bool,tf.ntemplates)
+	for i=1:tf.ntemplates
+		mm = tf.templates[:,:,i]
+		ii = indmax(-mm) #get the minimum point
+		vidx[i]= ii in div(size(mm,1),3):div(2*size(mm,1),3) #minimum should occur in the middle 3rd of the spike
+		vidx[i] = vidx[i] && -minimum(mm) >= maximum(mm) 
+	end
+	if any(vidx)
+		return TemplateFile(tf.templates[:,:,vidx], tf.cinv, sum(vidx), tf.nchannels)
+	else
+		return nothing
+	end
+end
+
+
