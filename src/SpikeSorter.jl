73cff7b856da2464de1a40793c0fb12cef322200
roger <roger.herikstad@gmail.com>
2017-05-17 11:16:17 +0800

    Adds spike extraction algorithms

--- src/SpikeSorter.jl
+++ src/SpikeSorter.jl
@@ -1,11 +1,13 @@
 module SpikeSorter
 using Compat
+using DSP
 include("types.jl")
 include("utility.jl")
 include("hmm.jl")
 include("viterbi.jl")
 include("readfiles.jl")
 include("features.jl")
+include("extraction.jl")
 import GUICheck
 if GUICheck.hasgui()
 	include("plot.jl")
